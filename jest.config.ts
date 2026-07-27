8046690873288335b0dd41dcd68d0831ca71302c
monds <elegbede.raymond@gmail.com>
2026-07-27 17:14:32 +0100

    fix: CI env vars take priority over dotenv in tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,15 @@
 import type { JestConfigWithTsJest } from "ts-jest"
 const config: JestConfigWithTsJest = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    roots: ["<rootDir>/tests"],
-    testMatch: ["**/*.test.ts"],
-    setupFiles: ["dotenv/config", "<rootDir>/tests/setEnv.ts"],
-    globalSetup: "<rootDir>/tests/globalSetup.ts",
-    testEnvironmentOptions: {},
-    moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1"
-    },
-    transformIgnorePatterns: ["node_modules/(?!(uuid)/)"]
-  }
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  testMatch: ["**/*.test.ts"],
+  setupFiles: ["<rootDir>/tests/setEnv.ts"],  // remove dotenv/config
+  globalSetup: "<rootDir>/tests/globalSetup.ts",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  transformIgnorePatterns: ["node_modules/(?!(uuid)/)"]
+}
 
 export default config
--- tests/setEnv.ts
+++ tests/setEnv.ts
@@ -1,2 +1,7 @@
-process.env.DATABASE_URL = process.env.TEST_DATABASE_URL!
-process.env.NODE_ENV = "test" 
+// only load .env file in local development
+if (!process.env.CI) {
+    require('dotenv').config()
+  }
+  
+  process.env.DATABASE_URL = process.env.TEST_DATABASE_URL ?? process.env.DATABASE_URL!
+  process.env.NODE_ENV = "test"
